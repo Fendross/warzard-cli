13172973df684cbeca74e2251585c641b461bae1
Fendross <dario.arzuffi@icloud.com>
2026-01-29 22:29:25 +0100

    Refactoring lore into a different module, although it would sit better in a Game struct.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-use std::{collections::HashMap};
+mod lore;
+
+use std::collections::HashMap;
 
 fn main() {
     println!("Warzard - He that does not wield magic in a magic world");
 
-    // Init lore data structure.
-    let lore: HashMap<i16, String> = get_lore_hash_set();
-    let str1 = lore.get(&1).expect("Default");
-    let str2 = lore.get(&2).expect("Default");
-
-    println!("{str1}\n{str2}");
-}
-
-// TODO Store lore into a JSON or YAML, using serde crate to serialize/deserialize it.
-fn get_lore_hash_set() -> HashMap<i16, String> {
-    let mut lore = HashMap::new();
-
-    lore.insert(1, "In a world where magic is everything, Vib was born with nothing.".to_string());
-    lore.insert(2, "Vib also wondered if this lore implementation is cool. He was not that convinced.".to_string());
+    // Init lore data.
+    let lore: HashMap<i16, String> = lore::init();
     
-    return lore
+    let str1 = lore::get_lore_from_index(&lore, 1);
+    let str2 = lore::get_lore_from_index(&lore, 2);
+
+    println!("{}\n{}", str1.unwrap(), str2.unwrap());
 }
